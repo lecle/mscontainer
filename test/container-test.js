cd02bc04699a43a1abdf676d13a2f8c561d77a7d
Daesun <yume@yumes.net>
2014-12-24 15:37:07 +0900

    skeleton #2 init simulation

---
+++ test/container-test.js
@@ -0,0 +1,16 @@
+var container = require('../lib/container');
+
+describe('container', function() {
+    describe('#init()', function() {
+        it('should initialize without error', function(done) {
+
+            // manager service load
+            container.init('', function(err) {
+
+
+                done(err);
+            });
+        });
+    });
+
+});
