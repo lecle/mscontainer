8e5b776fac6b3def8aa59ed9a5be0375afbbfb76
daesun <yume@yumes.net>
2015-01-08 17:44:33 +0900

    make a route table #3 getService, setRouteTable

---
+++ test/routeTable-test.js
@@ -0,0 +1,25 @@
+var routeTable = require('../lib/routeTable');
+
+describe('routeTable', function() {
+    describe('#getService()', function() {
+        it('should get a manager service without error', function(done) {
+
+            // test data
+            routeTable.setRouteTable({
+                'MANAGER': {
+                    'serviceName' : 'MANAGER',
+                    'serviceInfo' : {
+                        'moduleName' : 'manager-service',
+                        'ip' : '127.0.0.1',
+                        'port' : 80}
+                }
+            });
+
+            routeTable.getService('MANAGER', function(err) {
+
+                done(err);
+            });
+        });
+    });
+
+});
