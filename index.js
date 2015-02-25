3a2261155dcdd9e58c9d68d576484869aaf9cb2a
daesun <yume@yumes.net>
2015-02-25 18:11:04 +0900

    delete the test logic

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-module.exports = new (require('./lib/container'))();
-
-module.exports.init('', '', function() {});
-
-process.on('exit', module.exports.close);
+module.exports = require('./lib/container');
